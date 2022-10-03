dd33e89d293fbc90289cc421dcbea0ec56f3ba03
Baptiste PFEFFERKORN <bpfefferkorn@linksip.fr>
2022-10-04 00:41:43 +0200

    Part 4.5 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,10 +3,15 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  return blogs.reduce((acc, b) => acc + b.likes , 0)
+  return blogs.reduce((acc, b) => acc + b.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.sort((blogA, blogB) => blogB.likes - blogA.likes)[0]
 }
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
