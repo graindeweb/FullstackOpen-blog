718e20f04294d668c8745110aacc83d3ea7d8d43
Baptiste PFEFFERKORN <bpfefferkorn@linksip.fr>
2022-10-09 00:12:28 +0200

    Part 4.10 done

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,6 +3,7 @@ const Blog = require("../models/blogs")
 const supertest = require("supertest")
 const app = require("../app")
 const helper = require("./test_helper")
+const { text } = require("express")
 
 const api = supertest(app)
 
@@ -36,6 +37,41 @@ describe("GET blogs API", () => {
 
     expect(response.body[0].id).toBeDefined()
     expect(response.body[0]._id).not.toBeDefined()
+  })
+})
+
+describe("CREATE blog API", () => {
+  const newBlog = {
+    title: "Just a new empty blog",
+    author: "Baptiste PFEFFERKORN",
+    likes: 22,
+    url: "https://www.whatever.com/",
+  }
+
+  test("new blog is return as JSON", async () => {
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/)
+  })
+
+  test("new blog is added to blog list", async () => {
+    await api.post("/api/blogs").send(newBlog)
+    const { body: blogList } = await api.get("/api/blogs")
+
+    expect(blogList).toHaveLength(helper.initialBlogs.length + 1)
+    expect(blogList.map((b) => b.title)).toContain(newBlog.title)
+  })
+
+  test("new blog have correct values", async () => {
+    const { body: result } = await api.post("/api/blogs").send(newBlog)
+    const { author, url, title, likes } = await Blog.findById(result.id)
+
+    expect(author).toBe(newBlog.author)
+    expect(title).toBe(newBlog.title)
+    expect(url).toBe(newBlog.url)
+    expect(likes).toBe(newBlog.likes)
   })
 })
 
